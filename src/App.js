3d3fe23d458a10fc184e12f7182114266e337047
sonezakk@gmail.com <kosonekk123@gmail.com>
2020-04-11 22:26:16 +0700

    begin commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React  from 'react'
+import Nav from './component/nav'
+const App =() =>{
+ 
+   
+        // const [count, setCount] = useState(0);
+       
+        return (
+            <div>
+                <Nav/>
+                {/* <p>count : {count}</p>
+                <button onClick={()=>{setCount(count+1); console.log(count)} }>count</button> */}
+               
+            </div>
+        );
+    
 }
 
-export default App;
+export default App
