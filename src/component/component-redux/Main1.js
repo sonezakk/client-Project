61df918d10a70ea6b07e57362128921826d84b08
sonezakk@gmail.com <kosonekk123@gmail.com>
2020-04-27 03:12:08 +0700

    main

--- src/component/component-redux/Main1.js
+++ src/component/component-redux/Main1.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import './Main.css'
+import React from "react";
+import "./Main.css";
 export default function Main1() {
-    return (
-        <div className="con">
-            <div className="con-1">
-                <h1>แบ่งปันราคากุ้ง</h1>
-            <p>ทำไว้เพื่อสามารถให้เกษตรกรผู้เลี้ยงกุ้งสามารถเข้ามาแบ่งปันราคากุ้งของตนเองหรือตามตามราคากุ้งในแต่ละพื้นที่ที่จับไปในแต่</p>
-                </div>
-                <div className="con-2">
-                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, cum. Saepe laudantium iusto voluptatum corrupti nam facere esse laborum, architecto accusantium repellendus! Eligendi explicabo dolorem ex in rerum facilis quasi?</p>
-             </div>
-            </div>
-
-
-        
-    )
+  return (
+    <div className="con">
+      <div className="img1">
+        <img src="04.jpg" />
+      </div>
+      <div className="mainti">
+        <p>
+        สำหรับให้เกษตรกรผู้เลี้ยงกุ้งแบ่งปันราคาการชื้อขายกุ้งในแต่ละพื้นที่เพื่อเป็นการตัดสินใจในการขายกุ้งของเกษตรกร
+        เนื่องจากการชื้อขายในแต่ละพื้นที่ราคาไม่นอนขึ้นอยู่กับราคาตลาดและเจ้าของแพที่มาชื้อ
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/component/login/loginpage.js
+++ src/component/login/loginpage.js
@@ -35,27 +35,27 @@ const Loginpage = () => {
       <div className="shrimp">
         <img src="01.jpg" alt="" />
       </div>
-        <div className="onn">
-          <h1 className="title">แบ่งปันราคากุ้ง</h1>
-      <div className="box">
-        <div className="input">
-          <p1>Email </p1>
-          <input onChange={(e) => setEmail(e.target.value)} />
-          <div className="pass">
-            <p1>Password</p1>
-            <input
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
+      <div className="onn">
+        <h1 className="title">แบ่งปันราคากุ้ง</h1>
+        <div className="box">
+          <div className="input">
+            <p1>Email </p1>
+            <input onChange={(e) => setEmail(e.target.value)} />
+            <div className="pass">
+              <p1>Password</p1>
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="sub">
+            <p1 onClick={login}>Login</p1>
+          </div>
+          <div className="re">
+            <p1 onClick={signup}>Register</p1>
           </div>
         </div>
-        <div className="sub">
-          <p1 onClick={login}>Login</p1>
-        </div>
-        <div className="re">
-          <p1 onClick={signup}>Register</p1>
-        </div>
-      </div>
       </div>
     </div>
   );
