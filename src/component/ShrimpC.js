9934aa6c1572d8d0c647a3c9e92e5aa8339bb5ac
sonezakk@gmail.com <kosonekk123@gmail.com>
2020-04-29 22:47:26 +0700

    edit styled com

--- src/component/ShrimpC.js
+++ src/component/ShrimpC.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import './shrimpC.css'
+import React from "react";
+import "./shrimpC.css";
 
-const ShrimpC = props => {
-    return (
-        <div className="conten">
-       
-            
-            <div className="list">
-            <li className="id" >{props.id}</li>
-            <li> {props.day}</li>
-            <li className="city">{props.city}</li>
-            <li>{props.state}</li>
-            <li>{props.productshrimps}</li>
-            <li className="size"> {props.size}</li>
-            <li>{props.price}</li>
-           
-          
-            </div>
-            </div>
-        
-       
-    )
-}
+const ShrimpC = (props) => {
+  return (
+    <div className="conten5">
+      <div className="list5">
+        <li> {props.day}</li>
+        <li>{props.city}</li>
+        <li>{props.state}</li>
+        <li>{props.productshrimps}</li>
+        <li> {props.size}</li>
+        <li>{props.price}</li>
+      </div>
+    </div>
+  );
+};
 
 export default ShrimpC;
--- src/component/ShrimpL.js
+++ src/component/ShrimpL.js
@@ -1,61 +1,80 @@
+import React, { useState } from "react";
+import ShrimpC from "./ShrimpC";
 
-import React, { useState } from 'react';
-import ShrimpC from './ShrimpC';
-import './shrimpL.css'
-import styled from 'styled-components';
-const Noshrimps =styled.div`
-text-align:  center;
-padding-top:  10px;
-align-items: center;
-margin:  0;
-font-style: italic;
-text-shadow: 2px 2px rgb(163, 163, 163);
-`
+import styled from "styled-components";
+const Noshrimps = styled.div`
+  text-align: center;
+  padding-top: 10px;
+  align-items: center;
+  margin: 0;
+  font-family: 'Pridi', serif;
+  text-shadow: 2px 2px rgb(163, 163, 163);
+`;
+const Hshrimps = styled.div`
+  align-items: center;
+  // background-color :grey;
+  height: 880px;
 
+  padding-top: 10px;
+  .shrimpcontainer {
+    
+    list-style: none;
+   
+  }
+  .shrimpcontainer h1{
+    text-align: center;
+    color : black;
+    text-shadow: 2px 2px rgb(163, 163, 163);
+    font-family: 'Pridi', serif;
+  }
+  .listmanu-5 {
+    display: flex;
+    justify-content: space-around;
+
+    padding-left: 50px;
+    border : 2px solid black;
+    border-radius : 8px;
+    height : 50px;
+    align-items: center;
+    font-family: 'Pridi', serif;
+    background-color: #f2d8b3
+  }
+  .listmanu-5 li {
+    color: black;
+  }
+`;
 
 export default function ShrimpL(props) {
- 
-        
-    if (!props.shrimps || !props.shrimps.length)
-        {
-        return (<Noshrimps>
-            <h2>ไม่มีรายการ</h2>
-            </Noshrimps>)
-        }
-        
+  if (!props.shrimps || !props.shrimps.length) {
     return (
-        <div className='shrimpcontainer'>
-                <h1> รายการกุ้ง</h1>
-                
-                <div className="listmanu">
-                
-                     <li>รายการที่</li>
-                     <li>วันที่</li>
-                     <li>จังหวัด</li>
-                     <li>ตำแหน่ง</li>
-                     <li>ประเภทกุ้ง</li>
-                     <li>จำนวนไซต์</li>
-                     <li>ราคา</li>
+      <Noshrimps>
+        <h2>ไม่มีรายการ</h2>
+      </Noshrimps>
+    );
+  }
 
-                     
-                    
-                 
-                </div>
-          
+  return (
+    <Hshrimps>
+      <div className="shrimpcontainer">
+        <h1> รายการกุ้ง</h1>
 
-            {
-                
-                props.shrimps.map((shrimp, index) => (
-                    <div key={index} style={{ margin: 5 }}>
-                        <ShrimpC  {...shrimp} /> 
-                        
-                    </div>
-                ))
-            }
-            
-         
+        <div className="listmanu-5">
+          <li>วันที่</li>
+          <li>จังหวัด</li>
+          <li>ตำแหน่ง</li>
+          <li>ประเภทกุ้ง</li>
+          <li>จำนวนไซต์</li>
+          <li>ราคา</li>
         </div>
 
-    )
+        <div className="maplist5">
+          {props.shrimps.map((shrimp, index) => (
+            <div key={index} style={{ margin: 5 }}>
+              <ShrimpC {...shrimp} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </Hshrimps>
+  );
 }
-
